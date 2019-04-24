b0aa5efe89e5fe0c91720555223615e6e2ee11f3
sgolthi <sgolthi@DIN69000783.corp.capgemini.com>
2019-04-24 16:25:12 +0530

    "forth commit"

--- employee_management_mymain/src/main/java/com/cg/emp/MyMain.java
+++ employee_management_mymain/src/main/java/com/cg/emp/MyMain.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-//satya  
+//satya  golthi
 
 @SpringBootApplication
 @EnableAutoConfiguration
